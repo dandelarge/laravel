ffdb88810cae9926512352debab4020a25ecc343
DanDelarge <sdarauz@gmail.com>
2014-07-20 18:02:38 -0500

    version 0.1 -adding a migration

---
+++ app/database/migrations/2014_07_20_202617_authors.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class Authors extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
